0917c5d09783f8255bbeeaec9e2c23bcb6d0beb3
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-04-03 12:13:36 +0200

    Add files via upload

---
+++ ejercicioarraysjava94/main.js
@@ -0,0 +1,14 @@
+const letras = ['a', 'b', 'c', 'd', 'e'];
+
+console.log("Recorriendo el array de letras:");
+letras.forEach(letra => {
+    console.log(letra);
+});
+
+let numero = 5;
+
+console.log("\nIterando la variable 'numero' hasta que sea 0:");
+while (numero > 0) {
+    console.log(numero);
+    numero--;
+}
